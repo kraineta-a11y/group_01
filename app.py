0a1af85a520d6fffa4590259326868aad25cccf9
Neta Rosh <netarosh@Netas-MacBook-Air.local>
2026-01-17 12:41:13 +0200

    bug fixes

--- app.py
+++ app.py
@@ -151,9 +151,10 @@ def build_edit_flight_context(flight_number, error=None):
     cursor = conn.cursor(dictionary=True)
 
     cursor.execute("""
-        SELECT f.Flight_number, p.Size, f.Route_id, f.Departure_date, f.Departure_time, f.Flight_status, p.plane_id
+        SELECT f.Flight_number, p.Size, f.Route_id, f.Departure_date, f.Departure_time, f.Flight_status, p.plane_id, fr.Origin_airport, fr.Destination_airport
         FROM Flight f
         JOIN Plane p ON f.Plane_id = p.Plane_id
+        JOIN Flying_route fr ON f.Route_id = fr.Route_id
         WHERE f.Flight_number = %s
     """, (flight_number,))
     flight = cursor.fetchone()
