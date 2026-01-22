d658e6bf3f6289c6a7d27780754fbda276df33ba
Neta Rosh <netarosh1@users.noreply.github.com>
2026-01-22 16:46:22 +0200

    bug fix

--- app.py
+++ app.py
@@ -1897,17 +1897,17 @@ def manage_booking_result():
     # load seat prices
     for b in bookings:
         cursor.execute("""
-            SELECT fp.Price, st.Class_type, f.Departure_date, f.Departure_time, fr.Origin_airport, fr.Destination_airport
+            SELECT fp.Price, c.Class_type, f.Departure_date, f.Departure_time, fr.Origin_airport, fr.Destination_airport
             FROM Seats_in_order s
             JOIN Flight f ON f.Flight_number = %s
             JOIN Flying_route fr ON f.Route_id = fr.Route_id
-            JOIN Seat st ON st.Plane_id = s.Plane_id
-                     AND st.Row_num = s.Row_num
-                     AND st.Col_num = s.Col_num
+            JOIN Class c ON c.Plane_id = s.Plane_id
+                            AND s.Row_num BETWEEN c.first_row AND c.last_row
+                            AND s.Col_num BETWEEN c.first_col AND c.last_col
             JOIN Flight_pricing fp
                 ON fp.Flight_number = f.Flight_number
-               AND fp.Class_type = st.Class_type
-               AND fp.Plane_id = f.Plane_id
+            AND fp.Class_type = c.Class_type
+            AND fp.Plane_id = f.Plane_id
             WHERE s.Booking_number = %s
         """, (b['Flight_number'], b['Booking_number']))
         prices = cursor.fetchall()
